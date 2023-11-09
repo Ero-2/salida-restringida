dca36339c10ed046d616ff33755e60aed5674ed4
Ero-2 <125475081+Ero-2@users.noreply.github.com>
2023-11-09 00:53:57 -0600

    Agregar archivos de proyecto.

---
+++ salida restringida/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace salida_restringida
+{
+    class OutputRestrictedDeque<T>
+    {
+        private LinkedList<T> deque = new LinkedList<T>();
+
+        public void AddRear(T item)
+        {
+            deque.AddLast(item);
+        }
+
+        public T RemoveRear()
+        {
+            if (deque.Count == 0)
+            {
+                throw new InvalidOperationException("La bicola está vacía.");
+            }
+
+            T item = deque.Last.Value;
+            deque.RemoveLast();
+            return item;
+        }
+
+        public int Count()
+        {
+            return deque.Count;
+        }
+    }
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            OutputRestrictedDeque<int> deque = new OutputRestrictedDeque<int>();
+
+            deque.AddRear(1);
+            deque.AddRear(2);
+            deque.AddRear(3);
+
+            Console.WriteLine("Elemento trasero removido: " + deque.RemoveRear());
+
+            Console.WriteLine("Número de elementos en la bicola: " + deque.Count());
+
+            Console.ReadKey();
+        }
+
+
+    
+    }
+}
